5beb5bf593a8dfdd5a7f62a31f29b1ebd114b361
Sabrina Matos <sabrinamatosalmeida2@gmail.com>
2024-01-25 20:34:17 -0300

    A7

---
+++ index.js
@@ -0,0 +1,47 @@
+// var nomeProduto = "creme";
+// console.log(nomeProduto);
+// // Beatriz
+// nomeProduto = "batatinha";
+// // 3 tipos de variaveis
+// // variavel global -> todos dentro do arquivo tem acesso 
+// // proibido declarar o nome da variavel duas vezes 
+// var 
+
+// // variavel local -> apenas funciona no bloco de código criado
+// let
+
+// {
+//     let nome = "beatriz"
+// }
+
+// {
+//     let nome = "beatrizsas"
+//     console.log(nome)
+// }
+
+// // variavel constante -> o valor nao muda;
+// CEP 
+// const casa = 55424355;
+// const casa = 78453645;
+
+// // 3 tipos de dados 
+// // string -> dados textos | identificando string "" | ''
+// // number -> dados numericos
+// // booleanos -> dados verdadeiro ou falso | true or false
+
+// var nome = "Beatriz";
+// var idade = 23;
+// var maiorDeIdade = true;
+// var senha = "bia123#@yahoo.com";
+var valor1 = 3;
+var valor2 = 4
+console.log(valor1 + valor2);
+
+
+
+
+
+
+
+
+
